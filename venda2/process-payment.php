40186ecf7c1b8b0d22b0d9b6416ad4ce186e4aa2
Aurélio Maxwell <amax.a.roupa@gmail.com>
2025-04-18 12:44:55 +0200

    delete file

--- venda2/process-payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-     $paymentMethod = $_POST['paymentMethod'];
-    $additionalInput = isset($_POST['additionalInput']) ? $_POST['additionalInput'] : '';
-
-    echo "Método de Pagamento: $paymentMethod<br>";
-    echo "Valor Adicional: $additionalInput";
-}
-?>
